839bc324f452960e79710c57798cdafdbc382a05
Rajeev Singh <rajeevsinghece49@gmail.com>
2025-08-04 11:40:01 +0530

    Outputting list dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -35,15 +35,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            {/* Equivalent to passing title=..., description=..., and image=... manually, but done using object spread syntax */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
